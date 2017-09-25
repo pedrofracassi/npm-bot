2857f94e11cff6497ac9b57e900dd0eb437eb077
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-09-25 20:51:08 -0300

    Some quick changes

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ const expression = /([+][n][p][m])(\ *)(.*)/;
 function message(message) {
   if (message.content.match(expression)) {
     var embed = new Discord.RichEmbed();
+    embed.setColor(embedColor);
     var packagename = message.content.match(expression)[3].replace(' ', '-');
     if (packagename) {
-      message.channel.startTyping();
-      npmapi.getdetails(encodeURI(packagename), data => {
-        if (data.name) {
-          embed.setColor(embedColor);
-          embed.setAuthor(data.name, 'https://i.imgur.com/24yrZxG.png', 'https://www.npmjs.com/package/' + data.name);
-          embed.setDescription(data.description + '\nhttps://www.npmjs.com/package/' + data.name + '\n\n`npm install --save ' + data.name + '`');
-        } else {
-          embed.setColor(embedColor);
-          embed.setAuthor('No package with the name "' + packagename + '" was found on npm.', 'https://i.imgur.com/24yrZxG.png');
-          embed.setDescription('Please check if you spelled the package name correctly.');
-        }
-        message.channel.stopTyping();
+      if (packagename == '--help') {
+        embed.setDescription(`**Usage:** \`+npm <package name>\`\n\nIf you're having trouble with the bot, please open an [issue](https://github.com/pedrofracassi/npm-bot/issues)`);
         message.channel.send({embed});
-      });
+      } else if (packagename == '--invite') {
+        embed.setDescription(`[Invite me to your server by clicking this link](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot)\n\nI'm also open source, you can check my source code [here](https://github.com/pedrofracassi/npm-bot)`)
+        message.channel.send({embed});
+      } else {
+        message.channel.startTyping();
+        npmapi.getdetails(encodeURI(packagename), data => {
+          if (data.name) {
+            embed.setAuthor(data.name, 'https://i.imgur.com/24yrZxG.png', 'https://www.npmjs.com/package/' + data.name);
+            embed.setDescription(data.description + '\nhttps://www.npmjs.com/package/' + data.name + '\n\n`npm install --save ' + data.name + '`');
+          } else {
+            embed.setAuthor('No package with the name "' + packagename + '" was found on npm.', 'https://i.imgur.com/24yrZxG.png');
+            embed.setDescription('Please check if you spelled the package name correctly.');
+          }
+          message.channel.stopTyping();
+          message.channel.send({embed});
+        });
+      }
     } else {
-      embed.setColor(embedColor);
+
       embed.setAuthor('You need to specify a package name.', 'https://i.imgur.com/24yrZxG.png');
       embed.setDescription('**Usage:** `+npm <package name>`');
       message.channel.stopTyping();
